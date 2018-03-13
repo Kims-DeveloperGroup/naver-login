e6a304ecbce56b189172358e377d53da8db5c164
rikim <rikim@riotgames.com>
2018-03-13 23:32:16 +0900

    Add Client shutdown test

--- src/test/java/com/devoo/naverlogin/ParallelNaverClientTest.java
+++ src/test/java/com/devoo/naverlogin/ParallelNaverClientTest.java
@@ -34,4 +34,31 @@ public class ParallelNaverClientTest {
             fail("all items are not produced.");
         }
     }
+
+    @Test
+    public void shouldClientStopBeforeAllItemsAreConsumed() throws Exception {
+        //Given
+        BlockingQueue<String> inputs = new LinkedBlockingQueue<>();
+        BlockingQueue<String> outputs = new LinkedBlockingQueue<>();
+
+        for (int i = 0; i < 1000; i++) {
+            inputs.add(String.valueOf(i));
+        }
+        ClientAction<String, String> function = (s, client) -> s;
+
+        //When
+        ParallelNaverClient<String, String> parallelNaverClient = new ParallelNaverClient<>(1, inputs,
+                function, outputs);
+        parallelNaverClient.startAsyn();
+        Thread.sleep(50);
+        parallelNaverClient.stop();
+
+        //Then
+        if (inputs.isEmpty()) {
+            fail("all items are consumed.");
+        }
+        if (outputs.size() == 1000) {
+            fail("all items are produced.");
+        }
+    }
 }

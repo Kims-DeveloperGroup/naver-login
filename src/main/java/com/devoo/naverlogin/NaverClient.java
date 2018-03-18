e722eb2f4c671d2fae40f74ae10f9736e98a82a0
Kyeongsik Jason Kim <devookim@gmail.com>
2018-03-18 20:54:49 +0900

    Modify IsLogin to check current location at the very firist (#11)
    * Modify isLoginSucceeded() to check location at the very first Add loggis to NaverClient
    Add logging
    
    * Add NaverClient.terminate()

--- src/main/java/com/devoo/naverlogin/NaverClient.java
+++ src/main/java/com/devoo/naverlogin/NaverClient.java
@@ -7,8 +7,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NaverClient extends WebDriverUtilClient {
+    private static final Logger log = LoggerFactory.getLogger(NaverClient.class);
+
     static final String LOGIN_USERINFO_ELEMENT_CLASSNAME = "section_minime";
 
     private final String NAVER_HOME_URL = "https://www.naver.com/";
@@ -34,18 +38,21 @@ public class NaverClient extends WebDriverUtilClient {
         this.webDriver.findElement(By.id("id")).sendKeys(id);
         this.webDriver.findElement(By.id("pw")).sendKeys(password);
         this.webDriver.findElement(By.cssSelector(LOGIN_BUTTON_SELECTOR)).click();
+        log.debug("Trying to log in {}", id);
         if (!isLoginSucceeded()) {
+            log.debug("Loging fail : {}", id);
             throw new NaverLoginFailException();
         }
         return this;
     }
 
     public boolean isLoginSucceeded() {
-        new WebDriverWait(this.webDriver, 3)
-                .until(ExpectedConditions.visibilityOfElementLocated(By.className(LOGIN_USERINFO_ELEMENT_CLASSNAME)));
         if (!this.webDriver.getCurrentUrl().equals(NAVER_HOME_URL)) {
+            log.debug("The current url is not home: {}", this.webDriver.getCurrentUrl());
             return false;
         }
+        new WebDriverWait(this.webDriver, 3)
+                .until(ExpectedConditions.visibilityOfElementLocated(By.className(LOGIN_USERINFO_ELEMENT_CLASSNAME)));
         try {
             String classNameOfLoginElement =
                     webDriver.findElement(By.id("account")).getAttribute("class");
@@ -56,5 +63,9 @@ public class NaverClient extends WebDriverUtilClient {
             return false;
         }
         return false;
+    }
+
+    public void terminate() {
+        this.webDriver.quit();
     }
 }
